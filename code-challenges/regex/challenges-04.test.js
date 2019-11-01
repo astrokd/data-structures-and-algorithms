3370ed8cac1ba661d7e2370100b0729dee6cb0bb
AstroKD <kdreyerinseattle@gmail.com>
2019-10-31 23:53:06 -0700

    challenge 7

--- code-challenges/regex/challenges-04.test.js
+++ code-challenges/regex/challenges-04.test.js
@@ -112,7 +112,9 @@ Hint: All of these words end with the letters "ells".
 const seashells = 'She sells seashells by the seashore. The shells she sells are surely seashells. So if she sells shells on the seashore, I\'m sure she sells seashore shells.';
 
 const findShells = (str) => {
-  // Solution code here...
+  let regex = /\w+(ells)/g;
+  let resultMatch = str.match(regex);
+  return resultMatch;
 };
 
 /* ------------------------------------------------------------------------------------------------
