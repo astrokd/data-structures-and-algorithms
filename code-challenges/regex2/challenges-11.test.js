3d5e68112db3ae265f5e8140b88b962ae79e030a
AstroKD <kdreyerinseattle@gmail.com>
2019-11-11 23:04:01 -0800

    challenge 1

--- code-challenges/regex2/challenges-11.test.js
+++ code-challenges/regex2/challenges-11.test.js
@@ -9,7 +9,8 @@ If the PIN is four numerical digits long, return true. Otherwise, return false.
 ------------------------------------------------------------------------------------------------ */
 
 const validatePin = (pin) => {
-  // Solution code here...
+  let regex = /[0-9]{4}/g;
+  return regex.test(pin);
 };
 
 /* ------------------------------------------------------------------------------------------------
