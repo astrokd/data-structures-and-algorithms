d915404cd5e8b06840972f6bdae758c1e9fb462f
AstroKD <kdreyerinseattle@gmail.com>
2020-02-06 21:17:16 -0800

    object-inter chal 1

--- code-challenges/object-inter/challenges-06.test.js
+++ code-challenges/object-inter/challenges-06.test.js
@@ -13,8 +13,7 @@ const courseInfo = { name: 'Code 301', duration: { dayTrack: '4 weeks', eveningT
 };
 
 const getCourseKeys = (obj) => {
-  let properties = Object.keys(obj);
-  return properties;
+  return Object.keys(obj);
 };
 
 /* ------------------------------------------------------------------------------------------------
