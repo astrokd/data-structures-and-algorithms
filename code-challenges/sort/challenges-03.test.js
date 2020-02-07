18d55929f8b9a6d8392edbf8efc9a9a979557929
AstroKD <kdreyerinseattle@gmail.com>
2020-02-06 21:06:34 -0800

    sort chal 1

--- code-challenges/sort/challenges-03.test.js
+++ code-challenges/sort/challenges-03.test.js
@@ -7,10 +7,7 @@ Write a function named sortBackwards that takes in an array of numbers and retur
 ------------------------------------------------------------------------------------------------ */
 
 const sortBackwards = (arr) => {
-  arr.sort( (a, b) => {
-    return b - a;
-  });
-  return arr;
+  return arr.sort( (a, b) => {return b - a;});
 };
 
 /* ------------------------------------------------------------------------------------------------
