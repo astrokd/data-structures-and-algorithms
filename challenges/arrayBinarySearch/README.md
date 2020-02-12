9ea780310b0bfd141f5abf018ce773df27966576
AstroKD <kdreyerinseattle@gmail.com>
2020-02-12 14:11:24 -0800

    good algorithim for code

---
+++ challenges/arrayBinarySearch/README.md
@@ -0,0 +1,17 @@
+# Challenge Summary
+<!-- Short summary or background information -->
+Return the index of an array for a search key.
+
+## Challenge Description
+<!-- Description of the challenge -->
+Write a function called binarySearch that has 2 arguments, a sorted array
+and a search key, with built-ins return the index of the array is equal to the searchKey
+If the element doesn't exist return -1
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+
+
+## Solution
+<!-- Embedded whiteboard image -->
+![Whiteboard Image](./assets/binary-search.jpg)
---
+++ challenges/arrayBinarySearch/array-binary-search.js
@@ -0,0 +1,35 @@
+/* ----------------------
+array-binary-search
+
+Write a function called binarySearch that has 2 arguments, a sorted array
+and a search key, with built-ins return the index of the array is equal to the searchKey
+If the element doesn't exist return -1
+
+helped by https://www.geeksforgeeks.org/binary-search-in-javascript/
+
+For parameters:
+[4,8,15,16,23,42], 15 output: 2
+[11,22,33,44,55,66,77], 90 output: -1
+
+Input <--- sorted array, searchKey
+Output <--- number
+
+Calculate the middle index of the sorted array,
+If middle index is equal, return middle index,
+  Else if greater than searchKey, search the first half,
+  Loop over first half from middle to start.
+    if current value equals searchKey return current index,
+    else if first half current value is less than searchKey return -1.
+  Else if less than searchKey, search second half,
+  Loop over first half from middle to end.
+    if current value equals searchKey return current index,
+    else if first half current value is greater than searchKey return -1.
+Else return -1.
+
+
+---------------------- */
+
+let binarySearch = (sortedArray,searchKey) => {
+    const middleIndex = Math.round(sortedArray/2);
+
+}
