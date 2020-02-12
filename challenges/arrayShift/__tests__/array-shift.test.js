568085371f44cc95cdc44ab0f63c612360341ede
AstroKD <kdreyerinseattle@gmail.com>
2020-02-12 13:00:06 -0800

    added testing

--- challenges/arrayShift/__tests__/array-shift.test.js
+++ challenges/arrayShift/__tests__/array-shift.test.js
@@ -4,11 +4,16 @@ TESTS
 npm test challenges/arrayShift/array-shift.js
 ------------------------------------------------------------------------------------------------ */
 
-const Input = require('../array-shift.js');
+const insertShiftArray = require('../array-shift.js');
 
 describe('Array-Shift', () => {
-    test('It should return an array with a given value in the middle', () => {
-      expect(insertShiftArray([2,4,6,8], 5)).toStrictEqual([2,4,5,6,8]);
-      expect(insertShiftArray([4,8,15,23,42], 16).toStrictEqual([4,8,15,16,23,42]));
+  const orgArray = [2,4,6,8];
+  const updated = [2,4,5,6,8];
+  let emptyArray = [];
+  const newValue = 5;
+    it('It should return an array with a given value in the middle', () => {
+      emptyArray = insertShiftArray(orgArray,newValue);
+      expect(emptyArray).not.toEqual(orgArray);
+      expect(emptyArray).toEqual(updated);
     });
   });
