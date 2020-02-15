80c73a66f230cb8f348cc953e616418654550337
AstroKD <kdreyerinseattle@gmail.com>
2020-02-15 14:05:55 -0800

    mod js to get current test working, reorged files

---
+++ challenges/Data-Structures/linkedList/linked-list.test.js
@@ -0,0 +1,90 @@
+// Linked List
+// Solved with the help of: 
+// https://github.com/tylersayvetz/data-structures-and-algorithms/blob/linked-list/code-challenges/linked-list/linked-list.test.js
+
+// const LinkedList = require('../linked-list.js');
+// const Node = require('../linked-list.js');
+
+class Node {
+    constructor(value, next) {
+      this.value = value;
+      this.next = next;
+    }
+}
+  
+class LinkedList {
+    constructor(head = null) {
+      this.head = head;
+    }
+  
+    //Add to head
+    insert(value) {
+      if (!this.head) {
+          return 'No Head';
+      }
+      const newHead = new Node(value,this.head);
+      this.head = newHead;
+    }
+  
+    //
+    includes(value) {
+      if (!this.head) {return 'No Head';}
+      let current = this.head;
+      let result = false;
+      while(current.next !== null) {
+        current = current.next;
+        if (current.value === value) {
+            result = true;
+            return result;
+        }
+      }
+      return result;
+    }
+  
+    //
+    toString() {
+    if (!this.head) {
+      return 'No Head';
+    }
+    let current = this.head;
+    let str = '';
+    while(current.next !== null) {
+      str = str + `{ ${current.value} } -> `;
+      current = current.next;
+    }
+    return str + 'NULL';
+    }
+  
+}
+
+describe('linked-list insert', () => {
+    test('should insert value into linked-list', () => {
+        const headNode = new Node(0,null);
+        const linkedList = new LinkedList(headNode);
+        linkedList.insert(5);
+        expect(linkedList.head.value).toEqual(5);
+        expect(linkedList.head.next.next).toEqual(null);
+    });
+});
+
+describe('linked-list includes', () => {
+    test('should return boolean of value if it exist in list', () => {
+        const headNode = new Node(0,null);
+        const linkedList = new LinkedList(headNode);
+        linkedList.insert(5);
+        linkedList.insert(4);
+        linkedList.insert(3);
+        expect(linkedList.includes(4)).toEqual(true);
+        expect(linkedList.includes(8)).toEqual(false);
+    });
+});
+
+describe('linked-list includes', () => {
+    test('should return string of linked list', () => {
+        const headNode = new Node(0,null);
+        const linkedList = new LinkedList(headNode);
+        linkedList.insert(5);
+        linkedList.insert(0);
+        expect(linkedList.toString()).toEqual('{ 0 } -> { 5 } -> NULL');
+    });
+});
