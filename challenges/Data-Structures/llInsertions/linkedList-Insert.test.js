bd1a1a94a1a6e6c2ca4d8a49539bc090eb15b290
AstroKD <kdreyerinseattle@gmail.com>
2020-02-18 13:53:27 -0800

    insertBefore at head works insertAfter at tail doesnt

--- challenges/Data-Structures/llInsertions/linkedList-Insert.test.js
+++ challenges/Data-Structures/llInsertions/linkedList-Insert.test.js
@@ -66,11 +66,20 @@ class LinkedList {
       const newNode = new Node(newValue);
       while(current.next !== null) {
         if(current.value === value) {
-          const previousNode = current.prev;
-          current.prev.next = newNode; //assign the previous nodes next to newNode
-          newNode.next = current;
-          current.prev = newNode;
-          newNode.prev = previousNode;
+          const previousNode = current.prev || null;
+
+          if (previousNode === null) {
+            this.head = newNode;
+            newNode.next = current;
+            current.prev = newNode;
+            newNode.prev = null;
+          } else {
+            previousNode.next = newNode
+            newNode.next = current;
+            current.prev = newNode;
+            newNode.prev = previousNode;
+          }
+          
         }
         current = current.next;
       }
@@ -83,11 +92,19 @@ class LinkedList {
       const newNode = new Node(newValue);
       while(current.next !== null) {
         if(current.value === value) {
-          const nextNode = current.next
-          current.next = newNode;
-          newNode.prev = current;
-          newNode.next = nextNode;
-          nextNode.prev = newNode;
+          const nextNode = current.next || null;
+          // console.log('current.next',current.next);
+          if (nextNode === null) {
+            current.next = newNode;
+            newNode.prev = current;
+            newNode.next = null;
+          } else {
+            current.next = newNode;
+            newNode.prev = current;
+            newNode.next = nextNode;
+            nextNode.prev = newNode;
+          }
+
         }
         current = current.next;
       }
@@ -96,49 +113,6 @@ class LinkedList {
 }
 
 // -- TESTS ----------------------------------------------------
-
-// -- Singly Linked List
-
-describe('linked-list instantiate', () => {
-  test('should return the value of head', () => {
-      const headNode = new Node(0);
-      const linkedList = new LinkedList(headNode);
-      expect(linkedList.head.value).toEqual(0);
-      expect(linkedList.head.next).toEqual(null);
-  });
-});
-
-describe('linked-list insert', () => {
-    test('should insert value into linked-list', () => {
-        const headNode = new Node(0);
-        const linkedList = new LinkedList(headNode);
-        linkedList.insert(5);
-        expect(linkedList.head.value).toEqual(5);
-        expect(linkedList.head.next.next).toEqual(null);
-    });
-});
-
-describe('linked-list includes', () => {
-    test('should return boolean of value if it exist in list', () => {
-        const headNode = new Node(0);
-        const linkedList = new LinkedList(headNode);
-        linkedList.insert(5);
-        linkedList.insert(4);
-        linkedList.insert(3);
-        expect(linkedList.includes(4)).toEqual(true);
-        expect(linkedList.includes(8)).toEqual(false);
-    });
-});
-
-describe('linked-list toString', () => {
-    test('should return string of linked list', () => {
-        const headNode = new Node(0,null);
-        const linkedList = new LinkedList(headNode);
-        linkedList.insert('string');
-        linkedList.insert(true);
-        expect(linkedList.toString()).toEqual('{ true } -> { string } -> { 0 } -> NULL');
-    });
-});
 
 // -- Doubly Linked List
 
@@ -196,6 +170,21 @@ describe('linked-list insert before', () => {
   });
 });
 
+describe('linked-list insert before the first node of a linked list', () => {
+  test('should return the value of inserted items', () => {
+      const one = new Node(1);
+      const two = new Node(2);
+      const three = new Node(3);
+      one.next = two;
+      two.prev = one;
+      two.next = three;
+      three.prev = two;
+      const ll = new LinkedList(one);
+      ll.insertBefore(1,5);
+      expect(ll.head.value).toEqual(5);
+  });
+});
+
 describe('linked-list insert after', () => {
   test('should return the value of inserted items', () => {
       const one = new Node(1);
@@ -208,5 +197,22 @@ describe('linked-list insert after', () => {
       const ll = new LinkedList(one);
       ll.insertAfter(2,5);
       expect(two.next.value).toEqual(5);
+  });
+});
+
+describe('linked-list insert after the last node in a linked list', () => {
+  test('should return the value of inserted items', () => {
+      const one = new Node(1);
+      const two = new Node(2);
+      const three = new Node(3);
+      one.next = two;
+      two.prev = one;
+      two.next = three;
+      three.prev = two;
+      const ll = new LinkedList(one);
+      // console.log('toString()',ll.toString());
+      ll.insertAfter(3,5);
+      // console.log('toString()',ll.toString());
+      expect(three.next.value).toEqual(5);
   });
 });
