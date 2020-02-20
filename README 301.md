66859c498568767951164322ae6ae41e3fb66f2f
AstroKD <kdreyerinseattle@gmail.com>
2020-02-19 18:49:31 -0800

    readme setup

---
+++ README 301.md
@@ -0,0 +1,18 @@
+# data-structures-and-algorithms
+
+CodeFellows 301 Code Challenges
+
+- 01 -- for-each ✅ (Array.forEach())
+- 02 -- value-vs-Reference ✅ (Pass by Reference)
+- 03 -- sort ✅ (Sorting)
+- 04 -- regex ✅ (Regular Expressions 1)
+- 05 -- array-n-strings ✅ (slice(), splice(), split(), join())
+- 06 -- object-inter ✅ (Object Iteration)
+- 07 -- map ✅ (Array.map())
+- 08 -- filter ✅ (Array.filter())
+- 09 -- reduce ✅ (Array.reduce())
+- 10 -- chaining ✅ (Chaining Methods)
+- 11 -- regex2 ✅ (Regular Expressions 2)
+- 12 -- 2darray ✅ (Two-Dimensional Arrays)
+- 13 -- stringmanip ✅ (.includes(), .every(), .charAt())
+- 14 -- comboall ✅ (Combination of Everything)
---
+++ challenges/Data-Structures/llMerge/ll-merge.test.js
@@ -0,0 +1,152 @@
+// Linked List
+
+class Node {
+    constructor(value, next, prev) {
+      this.value = value;
+      this.next = next || null;
+      this.prev = prev || null;
+    }
+}
+  
+class LinkedList {
+    constructor(head) {
+      this.head = head;
+    }
+
+    // Append to tail
+    append(node) {
+      let current = this.head;
+      while(current.next !== null) {
+        current = current.next;
+      }
+        current.next = node;
+        node.prev = current;
+    }
+  
+    // Add to head
+    insert(value) {
+      if (!this.head) {return 'No Head';}
+      const oldHead = this.head;
+      const newHead = new Node(value,oldHead);
+      oldHead.prev = newHead;
+      this.head = newHead;
+    }
+  
+    // includes
+    includes(value) {
+      if (!this.head) {return 'No Head';}
+      let current = this.head;
+      let result = false;
+      while(current.next !== null) {
+        current = current.next;
+        if (current.value === value) {
+            result = true;
+            return result;
+        }
+      }
+      return result;
+    }
+  
+    // convert list values to string
+    toString() {
+      if (!this.head) {return 'No Head';}
+      let current = this.head;
+      let str = `{ ${current.value} } -> `;
+      while(current.next !== null) {
+        current = current.next;
+        str = str + `{ ${current.value} } -> `;
+      }
+      return str + 'NULL';
+    }
+
+    // insert before
+    insertBefore(value, newValue) {
+      if (!this.head) {return 'No Head';}
+      let current = this.head;
+      const newNode = new Node(newValue);
+      while(current.next !== null) {
+        if(current.value === value) {
+          const previousNode = current.prev || null;
+
+          if (previousNode === null) {
+            this.head = newNode;
+            newNode.next = current;
+            current.prev = newNode;
+            newNode.prev = null;
+          } else {
+            previousNode.next = newNode
+            newNode.next = current;
+            current.prev = newNode;
+            newNode.prev = previousNode;
+          }
+          
+        }
+        current = current.next;
+      }
+    }
+
+    // insert after
+    insertAfter(value, newValue) {
+      if (!this.head) {return 'No Head';}
+      let current = this.head;
+      const newNode = new Node(newValue);
+      while(current.next !== null) {
+        if(current.value === value) {
+          const nextNode = current.next || null;
+          // console.log('current.next',current.next);
+          if (nextNode === null) {
+            current.next = newNode;
+            newNode.prev = current;
+            newNode.next = null;
+          } else {
+            current.next = newNode;
+            newNode.prev = current;
+            newNode.next = nextNode;
+            nextNode.prev = newNode;
+          }
+
+        }
+        current = current.next;
+      }
+    }
+
+    //get kth from end
+    kthFromEnd(k) {
+      if (!this.head) {return 'No Head';}
+      let current = this.head;
+      let llCount = 0;
+      while(current.next !== null) {
+        llCount++;
+        current = current.next;
+      }
+      let count = 0;
+      current = this.head;
+      while(current.next !== null) {
+        if (count === llCount-k) {
+          return current.value;
+        }
+        current = current.next;
+        count++;
+      }
+      return current.value;
+    }
+  
+}
+
+// -- TESTS ----------------------------------------------------
+
+// -- Linked List kthFromEnd
+
+describe('linked-list kth from end', () => {
+  test('should return value of the kth from end in list', () => {
+      const one = new Node(5);
+      const two = new Node(6);
+      const three = new Node(7);
+      one.next = two;
+      two.prev = one;
+      two.next = three;
+      three.prev = two;
+      const ll = new LinkedList(one);
+      expect(ll.kthFromEnd(1)).toEqual(6);
+  });
+});
