720d2fd6a9967134c709e874680a2b1de54b4c2e
AstroKD <kdreyerinseattle@gmail.com>
2020-02-24 14:18:03 -0800

    push to stack test

--- challenges/stacksAndQueues/__tests__/stack.test.js
+++ challenges/stacksAndQueues/__tests__/stack.test.js
@@ -2,6 +2,13 @@ const {Stack} = require('../stacks-and-queues');
 
 // -- Stacks ------------------------------
 // push onto a stack
+describe('push onto a stack', () => {
+    const testStack = new Stack;
+    it('add new value to top of stack with push ', () => {
+        testStack.push('x');
+        expect(testStack.storage[0]).toEqual('x');
+    })
+})
 
 // push multiple values onto stack
 
