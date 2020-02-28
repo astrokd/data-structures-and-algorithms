b2e187d4e223f81d2c6d9fa8a117ef785db02eef
AstroKD <kdreyerinseattle@gmail.com>
2020-02-28 14:05:55 -0800

    layout

---
+++ challenges/tree/__tests__/tree.test.js
@@ -0,0 +1,14 @@
+//Test
+
+
+//Instantiate empty tree
+
+//Instantiate a tree with a single root node
+
+//Add a left child and right child to a single root node
+
+//return a collection of preorder traversal
+
+//return a collection of inorder traversal
+
+//return a collection of ipostorder
--- challenges/tree/tree.js
+++ challenges/tree/tree.js
@@ -1,1 +1,19 @@
 // Binary Tree and BST Implementation
+
+// Node Class
+class Node {
+  constructor(value, left = null, right = null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+//Create a BinaryTree class
+
+  //preOrder
+
+  //inOrder
+  
+  //postOrder
+
