5ff0f7230e20273bd298a438ca311b5d29da9da7
AstroKD <kdreyerinseattle@gmail.com>
2020-02-28 16:09:29 -0800

    preorder

--- challenges/tree/tree.js
+++ challenges/tree/tree.js
@@ -13,16 +13,42 @@ class Node {
 class BinaryTree {
   constructor() {
     this.root = null;
+    // this.preOrderOutput = [];
+    // this.inOrderOutput = [];
+    // this.postOrderOutput = [];
   }
 
-
-}
-
   //preOrder
+  preOrder() {
+    let stackArr = [];
+
+    let traverse = (node) =>  {
+      if (node === null) {
+        return null;
+      }
+      stackArr.push(node)
+      
+      if(node.left) {
+        traverse(node.left)
+      }
+      if(node.right) {
+        traverse(node.right)
+      }
+    }
+    return stackArr;
+
+  }
 
   //inOrder
+  inOrder(node) {
+
+  }
 
   //postOrder
+  postOrder(node) {
+
+  }
+}
 
 module.exports = {
   Node,
