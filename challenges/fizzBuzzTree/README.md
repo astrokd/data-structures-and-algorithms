30456339c39e4d343b9ed9cf0f5cf59e71bf3c5a
AstroKD <kdreyerinseattle@gmail.com>
2020-03-02 13:55:33 -0800

    tree setup

--- challenges/fizzBuzzTree/README.md
+++ challenges/fizzBuzzTree/README.md
@@ -14,7 +14,15 @@ Then return the new tree
 
 ## Approach & Efficiency
 <!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+Take tree and make a new one, Traverse new tree as a inordered list traversal and 
+- if value%15==0 value = 'FizzBuzz'
+- else if value%3==0 value = 'Fizz'
+- else if value%5==0 value = 'Buzz'
+- else if value = value.tostring
+return root node of tree
 
+- Time O(n) because we traverse the whole tree
+- Space is also 0(n) because we rewrite the whole tree.
 
 ## API
 <!-- Description of each method publicly available to your Stack and Queue-->
---
+++ challenges/fizzBuzzTree/fizz-buzz-tree.js
@@ -0,0 +1,45 @@
+// Node
+class Node {
+  constructor(value, left = null, right = null) {
+    this.value = value
+    this.left = left
+    this.right = right
+  }
+}
+
+//Binary Tree class
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
+
+  fizzBuzzTree() {
+    // make new tree
+    let newTree = new BinaryTree
+
+    // traverse tree
+    let traverse = (node) => {
+      if (node === null) {
+        return null
+      }
+      
+      if(node.left) {
+        traverse(node.left)
+      }
+      if(node.right) {
+        traverse(node.right)
+      }
+    }
+    traverse(this.root)
+
+  }
+
+}
+
+
+
+module.exports = {
+  Node,
+  BinaryTree,
+  fizzBuzzTree
+}
