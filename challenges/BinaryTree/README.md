888818e233772264aefbc79422298e5e4ed4a3a0
AstroKD <kdreyerinseattle@gmail.com>
2020-03-03 17:50:43 -0800

    base

---
+++ challenges/BinaryTree/README.md
@@ -0,0 +1,39 @@
+# Binary Tree and Breadth-First Traversal
+<!-- Short summary or background information -->
+Write a breadth first traversal method
+
+## Challenge
+<!-- Description of the challenge -->
+Write a breadth first traversal method which takes a Binary Tree as its unique input. Without utilizing any of the built-in methods available to your language, traverse the input tree using a Breadth-first approach, and return a list of the values in the tree in the order they were encountered.
+
+
+- [x] - Breadth First traversal method
+- [x] - Error handling
+- [x] - Test
+  - [x] - return a collection of breadth first traversal
+  - [x] - return a collection of breadth first traversal if tree is null
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+
+- `breadth-first`
+
+
+## API
+<!-- Description of each method publicly available to your Stack and Queue-->
+
+* Tree.js
+- Class Node
+  - property value
+  - property left
+  - property right
+- Class BinaryTree
+  - property root
+  - method preOrder
+  - method inOrder
+  - method postOrder
+
+
+[PR for Trees](https://github.com/astrokd/data-structures-and-algorithms/pull/43)
+
+[PR for Breadth First traversal](https://github.com/astrokd/data-structures-and-algorithms/pull/45)
---
+++ challenges/BinaryTree/__tests__/binarytree.test.js
@@ -0,0 +1,137 @@
+//Test
+const { Node,BinaryTree } = require('../tree')
+
+//Instantiate empty tree
+describe('Instantiate empty tree', () => {
+  const testTree = new BinaryTree
+  it('returns that tree is empty ', () => {
+    expect(testTree.root).toEqual(null)
+  })
+})
+
+//Instantiate a tree with a single root node
+describe('Instantiate  a tree with a single root node', () => {
+  const testTree = new BinaryTree
+  const testNode = new Node('A')
+  testTree.root = testNode
+  it('returns root of tree ', () => {
+    expect(testTree.root.value).toEqual('A')
+  })
+})
+
+//Add a left child and right child to a single root node
+describe('Instantiate a tree with a root, a left, and a right', () => {
+  const testTree = new BinaryTree
+  const rootNode = new Node('A')
+  const leftNode = new Node('B')
+  const rightNode = new Node('C')
+  rootNode.left = leftNode
+  rootNode.right = rightNode
+  testTree.root = rootNode
+  it('returns left of root of tree ', () => {
+    expect(testTree.root.left.value).toEqual('B')
+  })
+  it('returns right of root of tree ', () => {
+    expect(testTree.root.right.value).toEqual('C')
+  })
+})
+
+//return a collection of preorder traversal
+describe('Preorder Traversal', () => {
+  const testTree = new BinaryTree
+  const rootNode = new Node('A')
+  const bNode = new Node('B')
+  const cNode = new Node('C')
+  const dNode = new Node('D')
+  const eNode = new Node('E')
+  const fNode = new Node('F')
+  bNode.left = dNode
+  bNode.right = eNode
+  cNode.left = fNode
+  rootNode.left = bNode
+  rootNode.right = cNode
+  testTree.root = rootNode
+  let result = testTree.preOrder()
+  it('returns tree preorder traversed ', () => {
+    expect(result).toEqual(['A','B','D','E','C','F'])
+  })
+  const testNullTree = new BinaryTree
+  it('returns returns null for empty trees ', () => {
+    expect(testNullTree.preOrder()).toEqual(null)
+  })
+})
+
+//return a collection of inorder traversal
+describe('Inorder Traversal', () => {
+  const testTree = new BinaryTree
+  const rootNode = new Node('A')
+  const bNode = new Node('B')
+  const cNode = new Node('C')
+  const dNode = new Node('D')
+  const eNode = new Node('E')
+  const fNode = new Node('F')
+  bNode.left = dNode
+  bNode.right = eNode
+  cNode.left = fNode
+  rootNode.left = bNode
+  rootNode.right = cNode
+  testTree.root = rootNode
+  let result = testTree.inOrder()
+  it('returns tree inorder traversed ', () => {
+    expect(result).toEqual(['D','B','E','A','F','C'])
+  })
+  const testNullTree = new BinaryTree
+  it('returns returns null for empty trees ', () => {
+    expect(testNullTree.inOrder()).toEqual(null)
+  })
+})
+
+//return a collection of postorder
+describe('Postorder Traversal', () => {
+  const testTree = new BinaryTree
+  const rootNode = new Node('A')
+  const bNode = new Node('B')
+  const cNode = new Node('C')
+  const dNode = new Node('D')
+  const eNode = new Node('E')
+  const fNode = new Node('F')
+  bNode.left = dNode
+  bNode.right = eNode
+  cNode.left = fNode
+  rootNode.left = bNode
+  rootNode.right = cNode
+  testTree.root = rootNode
+  let result = testTree.postOrder()
+  it('returns tree postorder traversed ', () => {
+    expect(result).toEqual(['D','E','B','F','C','A'])
+  })
+  const testNullTree = new BinaryTree
+  it('returns returns null for empty trees ', () => {
+    expect(testNullTree.postOrder()).toEqual(null)
+  })
+})
+
+//return a collection of breadth first
+describe('Breadth FirstTraversal', () => {
+  const testTree = new BinaryTree
+  const rootNode = new Node('A')
+  const bNode = new Node('B')
+  const cNode = new Node('C')
+  const dNode = new Node('D')
+  const eNode = new Node('E')
+  const fNode = new Node('F')
+  bNode.left = dNode
+  bNode.right = eNode
+  cNode.left = fNode
+  rootNode.left = bNode
+  rootNode.right = cNode
+  testTree.root = rootNode
+  let result = testTree.breadthFirst()
+  xit('returns tree breadth first traversed ', () => {
+    expect(result).toEqual(['A','B','C','D','E','F'])
+  })
+  const testNullTree = new BinaryTree
+  xit('returns returns null for empty trees ', () => {
+    expect(testNullTree.pbreadthFirst()).toEqual(null)
+  })
+})
