4d135c5b3000f47abb79494fec60f593af5f01f5
AstroKD <kdreyerinseattle@gmail.com>
2020-03-31 15:51:37 -0700

    BFT of a graph base setup

---
+++ challenges/breadthFirst/breadth-first.js
@@ -0,0 +1,119 @@
+
+//Queue class
+class Queue {
+  constructor() {
+      this.front = null;
+      this.storage = [];
+  }
+
+  //enqueue
+  enqueue(value) {
+      if (this.front === null) {this.front = value}
+      return this.storage.push(value)
+  }
+
+  //dequeue
+  dequeue() {
+      if (this.front === null) {
+          return null;
+      } else {
+          if (this.storage[1] === undefined) {
+              this.front = null;
+          } else {
+              this.front = this.storage[1];
+          }
+          return this.storage.shift();
+      }
+  }
+
+  //peek
+  peek() {
+      return this.front;
+  }
+
+  //isEmpty
+  isEmpty() {
+      return this.storage.length ? false : true;
+  }
+}
+
+class Node{
+  constructor(value) {
+    this.value = value;
+  }
+}
+
+class Edge{
+  constructor(node, weight = 1){
+    this.node = node;
+    this.weight = weight;
+  }
+}
+
+class Graph{
+  constructor(){
+    this.adjacencyList = new Map();
+  }
+
+  //AddNode to the Map obj of the graph
+  addNode(node){
+    if (!this.adjacencyList.has(node)) {
+      return this.adjacencyList.set(node, []);
+    }
+  }
+
+  //AddEdge 
+  addEdge(node1, node2){
+    if (!this.adjacencyList.has(node1)) this.adjacencyList.set(node1, []);
+    if (!this.adjacencyList.has(node2)) this.adjacencyList.set(node2, []);
+
+    const neighborsOfNode1 = this.adjacencyList.get(node1);
+    const neighborsOfNode2 = this.adjacencyList.get(node2);
+
+    neighborsOfNode1.push(new Edge(node2));
+    neighborsOfNode2.push(new Edge(node1));
+  }
+
+  //GetNodes
+  getNodes(){
+    return [... this.adjacencyList.keys()];
+  }
+
+  //GetNeighbors
+  getNeighbors(node) {
+    if (!this.adjacencyList.has(node)){
+      return 'node does not exist in Graph'
+    }
+    return this.adjacencyList.get(node)
+  }
+
+  //Size
+  size(){
+    return this.adjacencyList.size;
+  }
+
+  //Breadth First Traversal
+  breadthFirst(node) {
+    let nodes = new Set()
+    let breadth = new Queue
+    breadth.enqueue(node)
+
+    while (!breadth.isEmpty) {
+      let front = breadth.dequeue()
+      nodes.add(front)
+
+      
+    }
+
+    return [... nodes.keys()]
+  }
+
+}
+
+module.exports = {
+  Queue,
+  Node,
+  Edge,
+  Graph
+}
+
---
+++ challenges/breadthFirst/breadth-first.test.js
@@ -0,0 +1,40 @@
+const { Queue, Node, Edge, Graph } = require ('./breadth-first')
+
+describe('graph', () => {
+  const testGraph = new Graph
+  const testNode = new Node(1)
+  it('Node can be successfully added to the graph', () => {
+    testGraph.addNode(testNode)
+    expect(testGraph.adjacencyList.get(testNode)).toEqual([])
+  })
+  it('An edge can be successfully added to the graph', () => {
+    const testNode2 = new Node(2)
+    testGraph.addEdge(testNode,testNode2)
+    expect(testGraph.adjacencyList.get(testNode)).toHaveLength(1)
+  })
+  it('A collection of all nodes can be properly retrieved from the graph', () => {
+    const result = [{"value": 1},{"value": 2}]
+    expect(testGraph.getNodes()).toEqual(result)
+  })
+  it('All appropriate neighbors can be retrieved from the graph', () => {
+    const neighbors = [{"node": {"value": 2},"weight": 1}]
+    expect(testGraph.getNeighbors(testNode)).toEqual(neighbors)
+  })
+  it('Neighbors are returned with the weight between nodes included', () => {
+    expect(testGraph.getNeighbors(testNode)[0].weight).toEqual(1)
+  })
+  it('The proper size is returned, representing the number of nodes in the graph', () => {
+    expect(testGraph.size()).toEqual(2)
+  })
+  it('A graph with only one node and edge can be properly returned', () => {
+    const singleGraph = new Graph
+    const singleNode = new Node(1)
+    const singleEdge = new Edge(singleNode)
+    singleGraph.addNode(singleNode)
+    expect(singleGraph.adjacencyList.has(singleNode)).toBeTruthy()
+  })
+  it('An empty graph properly returns null', () => {
+    const nullGraph = new Graph
+    expect(nullGraph.getNeighbors()).toEqual('node does not exist in Graph')
+  })
+})
