f9eaec846d9b3e5c9c605ddd65b6bfd3ac1aec0f
Kevin Dreyer <kdreyerinseattle@gmail.com>
2022-02-18 17:18:53 -0800

    updated readme with practices, did simple browserNav

--- practice/browserNav/browserNav.js
+++ practice/browserNav/browserNav.js
@@ -1,2 +1,41 @@
 // Design a system to track browser navigation history.
 
+/*
+
+input: URL and current LL of browser history
+output: Link list with all urls added
+
+write funtion that puts URL in LL
+
+*/
+
+const { Node,LinkedList } = require('../dataStructures/linkedLists/linked-lists')
+
+
+let addBrowserNav = (browserNavLL, navURL) => {
+    browserNavLL.push(navURL)
+    return browserNavLL
+}
+
+const one = new Node("www.google.com");
+const two = new Node("www.mozilla.com");
+const three = new Node("www.microsoft.com");
+
+
+one.next = two;
+two.previous = one;
+
+two.next = three;
+three.previous = two;
+
+let ll = new LinkedList(one)
+ll.length = 3
+ll.tail = three
+
+console.log('ll length: ', ll.length)
+console.log('ll tail value: ', ll.tail.value)
+console.log('ll head: ', ll.head)
+const addNavLL = addBrowserNav(ll, "www.linkedin.com")
+console.log("addNavLL length",addNavLL.length)
+console.log("addNavL tail value",addNavLL.tail.value)
+console.log("addNavL tail",addNavLL.tail)
